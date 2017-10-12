1915669ae2e3242b1c4435ec0d127451a0b15304
helinjiang <me@helinjiang.com>
2017-10-12 16:35:55 +0800

    feat: 异步请求页面的内容

--- src/aizhan-cha/now-qq-com.js
+++ src/aizhan-cha/now-qq-com.js
@@ -3,6 +3,7 @@ var utils = require('utils');
 var resultInfo = {};
 
 var casper = require('casper').create({
+    waitTimeout: 10 * 1000,
     clientScripts: ['../jquery.min.js', './client/now-util.js'],
     pageSettings: {
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36',
@@ -21,29 +22,53 @@ casper.start('https://www.aizhan.com/cha/now.qq.com/', function () {
     // 首先截个图
     this.capture('../tmp/1.png');
 });
+//
+// casper.then(function () {
+//     // 解析页面的内容
+//     var result = this.evaluate(function () {
+//         var obj = {};
+//
+//         // 页面标题
+//         obj.title = $('#webpage_title').text();
+//
+//         // SEO 信息
+//         obj.listSeoBasic = NOW_UTIL.getListSeoBasic();
+//
+//         // 收录变化等信息
+//         obj.listIndexChange = NOW_UTIL.getListIndexChange();
+//
+//         // 收录数目
+//         obj.listSiteRecord = NOW_UTIL.getListSiteRecord();
+//
+//         // 来路关键词
+//         obj.pcFromKeys = NOW_UTIL.getListPcFromKeys();
+//
+//         // META 关键词
+//         obj.getListMetaKeys = NOW_UTIL.getListMetaKeys();
+//
+//         return obj;
+//     });
+//
+//     resultInfo.data = result;
+// });
 
 casper.then(function () {
+    // 点击全部更新
+    this.click('#webpage_keywords_update a');
+});
+
+casper.wait(5000, function () {
+    this.echo('I\'ve waited for a second.');
+
+    // 截个图片看看
+    this.captureSelector('../tmp/3.png', '#webpage_keywords');
+
     // 解析页面的内容
     var result = this.evaluate(function () {
         var obj = {};
 
         // 页面标题
-        obj.title = $('#webpage_title').text();
-
-        // SEO 信息
-        obj.listSeoBasic = NOW_UTIL.getListSeoBasic();
-
-        // 收录变化等信息
-        obj.listIndexChange = NOW_UTIL.getListIndexChange();
-
-        // 收录数目
-        obj.listSiteRecord = NOW_UTIL.getListSiteRecord();
-
-        // 来路关键词
-        obj.pcFromKeys = NOW_UTIL.getListPcFromKeys();
-
-        // META 关键词
-        obj.getListMetaKeys = NOW_UTIL.getListMetaKeys();
+        obj.p1 = $('#keywords_postion_1').text();
 
         return obj;
     });
